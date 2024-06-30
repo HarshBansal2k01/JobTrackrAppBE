20b21b838eb61756dd97baaeb8c2f0620ca1b53a
HarshBansal2k01 <ihbbro14@gmail.com>
2024-06-30 13:28:12 +0530

    route

--- controller.js
+++ controller.js
@@ -162,7 +162,7 @@ const payment = async (req, res) => {
           quantity: item.quantity,
         };
       }),
-      success_url: "https://jobtrackrapp.netlify.app/success" ,
+      success_url: "https://github.com/HarshBansal2k01" ,
       cancel_url:  "https://jobtrackrapp.netlify.app/cancelled",
     });
 
